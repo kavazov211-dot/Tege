4d69bec70fdbf8360f04226a525c5890a9fc452b
kavazov211-dot <kavazov211@gmail.com>
2026-05-11 00:01:41 +0500

    Update aitgbot.py

--- aitgbot.py
+++ aitgbot.py
@@ -4,7 +4,7 @@ import edge_tts
 import asyncio
 import os
 
-TOKEN = "8435803353:AAEp9z8amY-M69uOk76DKS0ztd1Y-YozFN0"
+TOKEN = "7585451169:AAF5kBJjD2HjMzcEXTztr5UmCxRtvxqaT2Y"
 
 bot = telebot.TeleBot(TOKEN)
 
